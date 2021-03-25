4b712d9183f3c3363e29a0e183e8ee13ed683030
OFLH <oliviahill99992@gmail.com>
2021-03-25 12:07:29 +0000

    add playlist, searchbar, tracklist directories and files

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TrackList.css";
+import Track
+
+function TrackList() {
+  return (
+    <div className="TrackList">
+      {/* <!-- You will add a map method that renders a set of Track components  --> */}
+    </div>
+  );
+}
+
+export default TrackList;
